75a1f8003e540a7fba6e0efeb62e87c14eb713cf
Koustavjr <Koustavjr11@gmail.com>
2024-07-11 19:01:33 +0530

    store created using redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
+import { useState,useEffect } from "react"
+import authService from "./appwrite/auth"
+import Footer from "./components/Footer/Footer"
+import Header from "./components/Header/Header"
+import { Outlet } from "react-router-dom"
+import { login,logout } from "./store/authSlice"
+import { useDispatch } from "react-redux"
 
 
 function App() {
+  
+  const dispatch=useDispatch();
+  const [loading,setLoading]=useState(true)
 
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData)
+        dispatch(login({userData}))
+      else
+        dispatch(logout())
 
-  return (
-    <>
-     <h1>Hello World</h1>
-    </>
-  )
+    })
+    .finally(()=>{
+      setLoading(false)
+    })
+  },[])
+
+  return !loading? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header/>
+        <main>
+          <Outlet/>
+        </main>
+       <Footer/>
+      </div>
+    </div>
+  ):null
 }
 
 export default App
